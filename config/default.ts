bde720654f14b9f4462f76f2d251c914f3418bee
ivandjoh <ivandi.djoh@gmail.com>
2023-02-10 02:05:14 +0800

    reload

--- config/default.ts
+++
@@ -1,7 +0,0 @@
-export default {
-    port: 7000,
-    accessTokenExpiresIn: 15,
-    origin: 'http://localhost:7000',
-}
-
-// Credits : https://codevoweb.com/node-typescript-mongodb-jwt-authentication/
